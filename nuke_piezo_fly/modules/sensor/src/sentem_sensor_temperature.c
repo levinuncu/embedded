fa40f44ff6b326b545006e7c13684eb2a09b644a
Levin Uncu <levin@uncu.de>
2026-06-20 12:33:19 +0200

    fix log

--- nuke_piezo_fly/modules/sensor/src/sentem_sensor_temperature.c
+++ nuke_piezo_fly/modules/sensor/src/sentem_sensor_temperature.c
@@ -100,7 +100,7 @@ senaty_TemperatureSensorReading sentem_ReadData(void) {
     return kFailedReading;
   }
 
-  ESP_LOGI(kLoggerTag, "Read sensor data. Temperature %u, Humidity: %i", reading.humidity, reading.temperature);
+  ESP_LOGI(kLoggerTag, "Read sensor data. Temperature %u, Humidity: %i", reading.temperature, reading.humidity);
   return reading;
 }
 
